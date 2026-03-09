2fbe21f3573dde1275ca28a1229e8181ef956246
Rachel Yuan <rachelyuan@rachel-air.myfiosgateway.com>
2026-03-09 18:07:57 -0400

    Fixed path of website

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: "http://localhost:3000/auth/callback",
+        redirectTo: `${window.location.origin}/auth/callback`,
         queryParams: {
           prompt: "select_account",
         },
